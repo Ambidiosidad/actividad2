da855a95ed9b9e4da4050fcc896bfa165198da0e
jorge111 <48180592+Ambidiosidad@users.noreply.github.com>
2024-12-23 20:47:15 +0100

    Create justification.component.ts

---
+++ src/app/justification/justification.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-justification',
+  templateUrl: './justification.component.html',
+  styleUrls: ['./justification.component.css']
+})
+export class JustificationComponent {
+}
